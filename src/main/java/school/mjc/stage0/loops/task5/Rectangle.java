6dbe6bbb79289fa4be9525a7c40d9927b930ae3a
oleg <tsukanov.regula@gmail.com>
2023-01-13 00:37:42 +0500

    rectangle

--- src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Rectangle.java
@@ -3,5 +3,26 @@ package school.mjc.stage0.loops.task5;
 public class Rectangle {
     public void printRectangleFrom8s(int length, int height){
 
+        for (int i = 1; i <= height; i++) {
+
+            String side = "";
+            String emptySide = "";
+
+            for (int f = 1; f <= length; f++) {
+
+                side += 8;
+                if (f == 1 || f == length) {
+                    emptySide += 8;
+                } else {
+                    emptySide += " ";
+                }
+            }
+            if (i == 1 || i == height) {
+                System.out.println(side);
+            } else {
+                System.out.println(emptySide);
+            }
+        }
+
     }
 }
