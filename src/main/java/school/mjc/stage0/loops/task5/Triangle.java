687f4a5d5307a5f0910bae21319779c39ebad991
oleg <tsukanov.regula@gmail.com>
2023-01-12 21:06:53 +0500

    triangle

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -3,5 +3,14 @@ package school.mjc.stage0.loops.task5;
 public class Triangle {
     public void printTriangle(int cathetusLength) {
 
+        String side = "";
+
+        for (int i = 1; i <= cathetusLength; i++) {
+
+            side += 8;
+
+            System.out.println(side);
+        }
+
     }
 }
